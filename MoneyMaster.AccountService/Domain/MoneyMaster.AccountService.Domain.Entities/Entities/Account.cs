05240e03e417936b471879acb65946021312fae9
Marat <milya010790@mail.ru>
2025-01-08 13:52:25 +0500

    Добавлены сущности

---
+++ MoneyMaster.AccountService/Domain/MoneyMaster.AccountService.Domain.Entities/Entities/Account.cs
@@ -0,0 +1,38 @@
+﻿using MoneyMaster.Common.Entities.Base;
+using System.Transactions;
+
+namespace MoneyMaster.AccountService.Domain.Entities
+{
+    /// <summary>Аккаунт</summary>
+    /// <typeparam name="TKey">Тип первичного ключа</typeparam>
+    public class Account<TKey> : NamedTimedEntity<TKey>
+    {
+        /// <summary>Баланс</summary>
+        public decimal Balance { get; set; }
+
+        /// <summary>Расходы</summary>
+        public string? Currency { get; set; }
+
+        /// <summary>Иконка</summary>
+        public string? Icon { get; set; }
+
+        /// <summary>Идентификатор пользователя</summary>
+        public TKey? UserId { get; set; }
+
+        /// <summary>Мягкое удаление</summary>
+        public bool IsDelete { get; set; }
+
+        /// <summary>Идентификатор типа учетной записи</summary>
+        public TKey? AccountTypeId { get; set; }
+
+        /// <summary>Тип учетной записи</summary>
+        public AccountType? AccountType { get; set; }
+
+
+        /// <summary>Коллекция транзакций </summary>
+        public ICollection<Transaction>? Transactions { get; set; }
+    }
+
+    /// <summary><inheritdoc/></summary>
+    public class Account : Account<Guid> { }
+}
