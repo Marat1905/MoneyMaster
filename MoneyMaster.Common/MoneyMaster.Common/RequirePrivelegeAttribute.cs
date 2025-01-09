6bc0f98d2a9eff3ccc9d012c5e63fb4e23a96480
Marat <milya010790@mail.ru>
2025-01-09 15:36:20 +0500

    Для JWT токена

---
+++ MoneyMaster.Common/MoneyMaster.Common/RequirePrivelegeAttribute.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace MoneyMaster.Common
+{
+    /// <summary>
+    ///  Привилегии доступные для назначения пользователю, если нужно добавить - добавляем сюда
+    ///  В базу данных пишем в поле Role соответствующую роль в текстовом формате
+    /// </summary>
+    [Flags]
+    public enum Priviliges
+    {
+        System = 0,
+        Administrator = 1,
+        User,
+        Manager,
+    }
+
+    /// <summary>
+    /// Атрибут позволяющий разграничить доступ по нескольким ролям, агрегатор атрибута Authorize
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true)]
+    public class RequirePrivelegeAttribute : AuthorizeAttribute
+    {
+        public RequirePrivelegeAttribute(params Priviliges[] priviliges)
+        {
+            Roles = String.Join(",", priviliges);
+        }
+
+    }
+}
